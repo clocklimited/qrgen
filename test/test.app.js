cbfd9fec8411346fec79c137a5b3fb5cb06de62e
Josh Wilson <Josh.wilson@clock.co.uk>
2012-11-22 14:16:27 +0000

    Better Tests for api

--- test/test.app.js
+++ test/test.app.js
@@ -1,42 +1,36 @@
 // Packages
-var http = require('http')
+var request = require('request')
   , properties = require('../properties')
-
-// Post function to send data to api
-function postAction(query) {
-
-  if(query) {
-    query = '?data=' + query
-  }
-
-  var options = {
-    host: properties.url,
-    path: '/v1/qr' + query,
-    port: properties.port,
-    method: 'POST'
-  }
-
-  var callback = function(response) {
-    var str = ''
-    response.on('data', function (chunk) {
-      str += chunk
-    });
-
-    response.on('end', function () {
-      console.log(str)
-    })
-  }
-
-  var req = http.request(options, callback)
-  req.write();
-  req.end();
-}
 
 // Mocha Tests
 describe('app.js', function() {
+
     it('should error with POST data is missing', function() {
-      var error
-      error.postAction().should.throwError('Error: POST data is missing')
+      request(properties.url + ':' + properties.port + '/qr', function (error, response, body) {
+        body.should.be.equal('Error: POST data is missing')
+      })
+    })
+
+    it('should error with 500 status code', function() {
+      request(properties.url + ':' + properties.port + '/qr', function (error, response, body) {
+        body.should.be.equal('500')
+      })
+    })
+
+    it('should return with 200 status code', function() {
+      request(properties.url + ':' + properties.port + '/qr?data=http://www.example.com', function (error, response, body) {
+        body.should.be.equal('200')
+      })
+    })
+
+    it('should return with correct base64 Image', function() {
+      request(properties.url + ':' + properties.port + '/qr?data=http://www.example.com', function (error, response, body) {
+
+        // Contorl Data for http://www.example.com
+        var controlData = 'iVBORw0KGgoAAAANSUhEUgAAAJwAAACcCAIAAAAF2lUaAAAABmJLR0QA/wD/AP+gvaeTAAADVklEQVR4nO3dwYrrMBBE0efH/P8vZ3aDsxCo6S5bXO5ZDo7jSdE0siX5+nw+/8Ty/+0L0DxDBTJUIEMFMlQgQwUyVCBDBTJUIEMFMlQgQwUyVCBDBTJUIEMFMlQgQwUyVKCfzoev65q6jj+rOVP376oes/P3nevpnKeqM3fMSgUyVCBDBWr11LtOD6j2ts5nq7252i/Tv8MOKxXIUIEMFWisp951emTC6npW/bXTv6vHJ34HKxXIUIEMFSjSU59U7X9T941PZqUCGSqQoQId3VNP62enXc+KlQpkqECGChTpqVP9pjNenBq/drzVd61UIEMFMlSgsZ6amPvamcfbma/bmd+b+B2qrFQgQwUyVKDr5HuYO2PKqXlAp82r6rBSgQwVyFCBHp33++R6mNX5O+tqdv5+wvwmKxXIUIEMFeiIPR+qfWjq+Oq1pU31YCsVyFCBDBWo1VMTzxqn1oauPtsZ11Yl7lHvsFKBDBXIUIEiz1Orc3zS93VXEs9ZO/3YcaqWDBXIUIHi8347+/TufFdnXU3CVJ/usFKBDBXIUIFa49SpNS3V79qRWGPTWfP65JwmKxXIUIEMFei1tTRTfWh1zsQc453jd76r8z/usFKBDBXIUIFeW0uT2Ke+Mx84cQ3OUdIYQwUyVKCxcepbY8Sqt+7xVs/vOFVfDBXIUIGO2EN/ah+GHYl1MtVjqu+Yq7JSgQwVyFCBHp2jtHOelamx7470fsLpZ9hWKpChAhkqUHyO0pP7FHaO2bmGqe/dOd57v/piqECGCnTEe2nS83sT7y1fqfbaxDNjKxXIUIEMFWispyaeTU7Nv5065131nnb6OfGdlQpkqECGCnTEHvp36XnI1fendr7r7snnrFYqkKECGSrQEePUqbFg4v5qohfuXKfPU/XFUIEMFejRPR/upvYmfOt9pU/eQ66yUoEMFchQgY5Yn7rSGYNO9eCp95mnx6Z3ViqQoQIZKtDRPfVuapyXeE/OylQPrrJSgQwVyFCBIj11arxVnR+b2GsifbzjVG0xVCBDBTpifWpVYm5t593mb+2XtGKlAhkqkKECvfauN+VYqUCGCmSoQIYKZKhAhgpkqECGCmSoQIYKZKhAhgpkqECGCmSoQIYKZKhAhgpkqEC/IoQjdAT3NoYAAAAASUVORK5CYII='
+
+        body.should.be.equal(controlData)
+      })
     })
 
 })
